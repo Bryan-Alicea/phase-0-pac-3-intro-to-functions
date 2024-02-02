818fe81827384accf2cb3a799b01792598843e3f
Bryan <aliceabryan1996@gmail.com>
2024-02-01 22:10:05 -0500

    practice

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Follow along with the examples here
+function doNothing() {
+    console.log("Hello, Harry Potter!")
+}
+
+doNothing()
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!")
+}
+
+sayHelloToGuadalupe()
+
+function doSomething(thing){
+    console.log(thing)
+}
+
+doSomething("Eat a pancake")
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}`)
+}
+
+say("Welcome to the Jungle", "Steven")
+
+function add(num1, num2) {
+    return num1 + num2;
+  }
+  
+  const sum = add(55, 6);
+  const message = `The sum of your numbers is: ${sum}.`;
+  
+  console.log(message);
+
+  
